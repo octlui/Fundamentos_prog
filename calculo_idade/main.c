2c39b65f85e58bbcd26ca477196b6719baa7e7f0
Luiza <luizarriocampos@gmail.com>
2025-05-03 18:06:47 -0300

    Versão 2 - ajuste de inicialização

--- calculo_idade/main.c
+++ calculo_idade/main.c
@@ -7,10 +7,12 @@ Pessoa Newton, Einstein;
     Newton.dia= 4;
     Newton.mes= 1;
     Newton.ano= 1643;
+    Newton.idade= -1; //incializar com -1 sempre
 
     Einstein.dia = 14;
     Einstein.mes = 3;
     Einstein.ano = 1879;
+    Einstein.idade = -1;
 
     Newton.idade = calc_idade(Newton, 11, 1, 2009);
     Einstein.idade = calc_idade(Einstein, 11, 1, 2009);
