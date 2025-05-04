ef1d4254761e58dabb44dff7e8c90806b728109d
Luiza <luizarriocampos@gmail.com>
2025-05-04 15:37:13 -0300

    Atribuindo private no pessoa.h

--- calculo_idade/main.cpp
+++ calculo_idade/main.cpp
@@ -8,8 +8,8 @@ int main(){
     Einstein.calc_idade(11, 1, 2009); //tanto einstein quanto newton tem seu próprio calculo_idade
     Newton.calc_idade(11, 1, 2009); 
 
-    printf("A idade de Newton seria de %d \n", Newton.idadeP);
-    printf("A idade de Einstein seria de %d \n", Einstein.idadeP);
+    printf("A idade de Newton seria de %d \n", Newton.informaIdade());
+    printf("A idade de Einstein seria de %d \n", Einstein.informaIdade());
 
     getchar(); //dá standby na tela
     return 0;
--- calculo_idade/pessoa.h
+++ calculo_idade/pessoa.h
@@ -1,27 +1,31 @@
 #include <stdio.h>
 struct Pessoa{
-    public:
-        int diaP, mesP, anoP, idadeP; //informações acessíveis de fora;
+    private:
+        int diaP, mesP, anoP, idadeP; //atributos privados não podem ser utilizados fora do escopo
     
     //função construtora inicializa as variaveis, pega as variaveis do main e atribui a elas os atributos dos objetos
-    Pessoa(int diaNa, int mesNa, int anoNa){
-        diaP = diaNa;
-        mesP = mesNa;
-        anoP= anoNa;
-        idadeP = -1;
-    }
+    public:
+        Pessoa(int diaNa, int mesNa, int anoNa){
+            diaP = diaNa;
+            mesP = mesNa;
+            anoP= anoNa;
+            idadeP = -1;
+        }
 
-    //função dentro da struct
-    void calc_idade(int diaAT, int mesAT, int ano_atualAT){ 
-    idadeP = ano_atualAT - anoP;                            
-    if(mesAT < mesP){
-            idadeP -= 1; //se a pessoa não fez aniversário, é -1 ano;
-        }else{
-            if(mesP == mesAT){
-                if(diaP > diaAT){
-                    idadeP -= 1;
+        //função dentro da struct
+        void calc_idade(int diaAT, int mesAT, int ano_atualAT){ 
+        idadeP = ano_atualAT - anoP;                            
+        if(mesAT < mesP){
+                idadeP -= 1; //se a pessoa não fez aniversário, é -1 ano;
+            }else{
+                if(mesP == mesAT){
+                    if(diaP > diaAT){
+                        idadeP -= 1;
+                    }
                 }
-            }
+        }
     }
-}
+        int informaIdade(){
+            return idadeP;
+        }
 };
