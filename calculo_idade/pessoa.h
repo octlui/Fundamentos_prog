13d132850349c7e8f99db3906c48a4ebd0c1bb19
Luiza <luizarriocampos@gmail.com>
2025-05-04 15:51:25 -0300

    Implementação da classe + renomeação

--- calculo_idade/pessoa.h
+++ calculo_idade/pessoa.h
@@ -1,19 +1,19 @@
 #include <stdio.h>
-struct Pessoa{
-    private:
-        int diaP, mesP, anoP, idadeP; //atributos privados não podem ser utilizados fora do escopo
+class Pessoa{ //classe representa uma estrutura geral de um conjunto de elementos
+    private:    //em uma classe os elementos são private por definição (em uma struct, são public por definição)
+        int diaP, mesP, anoP, idadeP; 
     
     //função construtora inicializa as variaveis, pega as variaveis do main e atribui a elas os atributos dos objetos
     public:
         Pessoa(int diaNa, int mesNa, int anoNa){
             diaP = diaNa;
-            mesP = mesNa;
+            mesP = mesNa;   //as variaveis dentro de uma classe são chamadas de atributos
             anoP= anoNa;
             idadeP = -1;
         }
 
         //função dentro da struct
-        void calc_idade(int diaAT, int mesAT, int ano_atualAT){ 
+        void calc_idade(int diaAT, int mesAT, int ano_atualAT){ //funções dentro de uma classe são chamadas de métodos  
         idadeP = ano_atualAT - anoP;                            
         if(mesAT < mesP){
                 idadeP -= 1; //se a pessoa não fez aniversário, é -1 ano;
