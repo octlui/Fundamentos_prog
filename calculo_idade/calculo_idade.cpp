0344bd8379715496f202185f3fccad0956325a0a
Luiza <luizarriocampos@gmail.com>
2025-05-04 14:52:20 -0300

    Cpp com objetos de pessoa

--- calculo_idade/calculo_idade.cpp
+++ calculo_idade/calculo_idade.cpp
@@ -1,38 +1,38 @@
 #include <stdio.h>
 typedef struct{
-    int dia, mes, ano, idade;
-}Pessoa;
+    int diaP, mesP, anoP, idadeP;
 
-void calc_idade(Pessoa& p, int dia, int mes, int ano_atual){ //referencia escondida funciona como um ponteiro mas só na função
-    p.idade = ano_atual - p.ano;                            //não preciso usar a sintaxe de ponteiros
-    if(mes < p.mes){
-            p.idade -= 1; //se a pessoa não fez aniversário, é -1 ano;
+    void calc_idade(int diaAT, int mesAT, int ano_atualAT){ 
+    idadeP = ano_atualAT - anoP;                            
+    if(mesP < mesAT){
+            idadeP -= 1; //se a pessoa não fez aniversário, é -1 ano;
         }else{
-            if(p.mes == mes){
-                if(p.dia > dia){
-                    p.idade -= 1;
+            if(mesP == mesAT){
+                if(diaP > diaAT){
+                    idadeP -= 1;
                 }
             }
     }
 }
+}Pessoa;
 int main(){
-    Pessoa Newton, Einstein;
+    Pessoa Newton, Einstein; //agora são objetos do tipo pessoa
     
-    Newton.dia= 4;
-    Newton.mes= 1;
-    Newton.ano= 1643;
-    Newton.idade = -1; //inicializar qualquer variável como -1
+    Newton.diaP= 4;
+    Newton.mesP= 1;
+    Newton.anoP= 1643;
+    Newton.idadeP = -1; //inicializar qualquer variável como -1
 
-    Einstein.dia = 14;
-    Einstein.mes = 3;
-    Einstein.ano = 1879;
-    Einstein.idade = -1;
+    Einstein.diaP = 14;
+    Einstein.mesP = 3;
+    Einstein.anoP = 1879;
+    Einstein.idadeP = -1;
 
-    calc_idade(Newton, 11, 1, 2009);
-    calc_idade(Einstein, 11, 1, 2009); //não preciso colocar o &comercial, a variável entra e é modificada na função
+    Einstein.calc_idade(11, 1, 2009); //tanto einstein quanto newton tem seu próprio calculo_idade
+    Newton.calc_idade(11, 1, 2009); 
 
-    printf("A idade de Newton seria de %d \n", Newton.idade);
-    printf("A idade de Einstein seria de %d \n", Einstein.idade);
+    printf("A idade de Newton seria de %d \n", Newton.idadeP);
+    printf("A idade de Einstein seria de %d \n", Einstein.idadeP);
 
     getchar(); //dá standby na tela
     return 0;
