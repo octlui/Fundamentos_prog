27200838563c0d394dfdcb52ab9af2e8bcf04bfa
Luiza <luizarriocampos@gmail.com>
2025-05-04 15:25:02 -0300

    Organizando em pessoa.h e main.cpp

---
+++ calculo_idade/main.cpp
@@ -0,0 +1,16 @@
+#include "pessoa.h"
+
+int main(){
+    
+    Pessoa Einstein (14, 3, 1879);
+    Pessoa Newton (4, 1, 1643);
+
+    Einstein.calc_idade(11, 1, 2009); //tanto einstein quanto newton tem seu próprio calculo_idade
+    Newton.calc_idade(11, 1, 2009); 
+
+    printf("A idade de Newton seria de %d \n", Newton.idadeP);
+    printf("A idade de Einstein seria de %d \n", Einstein.idadeP);
+
+    getchar(); //dá standby na tela
+    return 0;
+}
